24c9ce778f36d86c3692a557dcaf1eb804f25fc3
tommylhw <62752366+tommylhw@users.noreply.github.com>
2025-08-16 10:45:39 +0800

    setup babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -9,7 +9,7 @@ module.exports = function (api) {
             // all files under this folder will be processed by the Babel plugin
             // if you need to include more folders, or customize discovery process
             // check available babel options
-            root: 'src'
+            root: './'
         }]
     ]
   };
---
+++ unistyles.ts
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native-unistyles";
+const lightTheme = {
+  colors: {
+    primary: "#ff1ff4",
+    secondary: "#1ff4ff",
+    // any nesting, spreading, arrays, etc.
+  },
+};
+
+const darkTheme = {
+  colors: {
+    primary: "#ff1ff4",
+    secondary: "#1ff4ff",
+    // any nesting, spreading, arrays, etc.
+  },
+};
+
+const appThemes = {
+  light: lightTheme,
+  other: darkTheme,
+};
+
+type AppThemes = typeof appThemes;
+
+declare module "react-native-unistyles" {
+  export interface UnistylesThemes extends AppThemes {}
+}
+
+StyleSheet.configure({
+  themes: appThemes,
+  settings: {
+    initialTheme: "light",
+  },
+});
